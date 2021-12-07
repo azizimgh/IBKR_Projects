641388c34c2618a6496f623add922a5ea8ef935b
mac <imghaziz@gmail.com>
2021-12-07 22:38:52 +0100

    First commit

--- Billy_Fort/config.py
+++ Billy_Fort/config.py
@@ -1,10 +1,12 @@
 
 
-account_type = "live" # it can be either live or testing
+account_type = "testing" # it can be either live or testing
 
 white_list = ["TSLA"] # stocks to leave alone even when they meet liquidation condition
 
-liquidation_treshhold = -10 #usd
+liquidation_treshhold = -10 #usd this is for stocks
+
+liquidation_treshhold_options = -10 #usd
 
 price_update_frequency = 10 #sends ,  mininum 10 seconds
 
--- Billy_Fort/main_ib.py
+++ Billy_Fort/main_ib.py
@@ -1,8 +1,8 @@
 import time
 from ib_insync import *
 
-from Billy_Fort.config import position_update_frequency, price_update_frequency, liquidation_treshhold, white_list, \
-    account_type
+from config import position_update_frequency, price_update_frequency, liquidation_treshhold, white_list, \
+    account_type, liquidation_treshhold_options
 
 util.startLoop()
 
@@ -21,21 +21,46 @@ ticks = {}
 avgs = {}
 pos = {}
 seen = []
+
+ticks_opt = {}
+avgs_opt = {}
+pos_opt = {}
+seen_opt = []
+
+
+def sell_option(contract, pos):
+    msft_option_contract = contract
+    ib.qualifyContracts(msft_option_contract)
+    msft_option_order = MarketOrder('SELL', pos)
+    msft_option_trade = ib.placeOrder(msft_option_contract, msft_option_order)
+
+
+
 while True:
     for elt  in ib.positions(account='DU3424271'):
         try:
             ctr = elt.contract
+            if elt.contract.secType =="STK":
+                if ctr not in ticks.keys():
+                    tic = ib.reqMktData(Option(ctr.symbol, ctr.lastTradeDateOrContractMonth, ctr.strike, ctr.right, 'SMART')) #
+                    ticks_opt[ctr] = tic
+                id = elt.contract.conId
+                position = elt.position
+                avg_cost = elt.avgCost
+                avgs[ctr] = avg_cost
+                pos[ctr] = position
+                #print(id,position,avg_cost,elt)
 
-            if ctr not in ticks.keys():
-                tic = ib.reqMktData(Stock(ctr.symbol, 'SMART', 'USD'))
-                ticks[ctr] = tic
-            id = elt.contract.conId
-            position = elt.position
-            avg_cost = elt.avgCost
-            avgs[ctr] = avg_cost
-            pos[ctr] = position
-            #print(id,position,avg_cost,elt)
-
+            if elt.contract.secType =="OPT":
+                if ctr not in ticks.keys():
+                    tic = ib.reqMktData(Stock(ctr.symbol, 'SMART', 'USD'))
+                    ticks[ctr] = tic
+                id = elt.contract.conId
+                position = elt.position
+                avg_cost = elt.avgCost
+                avgs[ctr] = avg_cost
+                pos[ctr] = position
+                #print(id,position,avg_cost,elt)
         except Exception as e:
             print("An error has occured: ", e)
 
@@ -56,9 +81,24 @@ while True:
             elif pnl < liquidation_treshhold and elt.symbol not in seen and elt.symbol  in white_list:
                 print("      This stock is white listed")
 
-            elif pnl < liquidation_treshhold and elt.symbol in seen and elt.symbol :
+            elif pnl < liquidation_treshhold and elt.symbol in seen :
                 print("      This position is already closed")
 
+        for elt in ticks_opt.keys():
+            lst =  ticks_opt[elt].last
+            pnl = pos_opt[elt]*(lst - avgs[elt])
+            print(elt.symbol, ' Total positions: ',pos[elt]," Last price: ",ticks_opt[elt].last," PNL: " ,pnl)
+
+            if pnl < liquidation_treshhold_options and elt.symbol not in seen_opt and elt.symbol not in white_list:
+                print("      This is a loosing position , closing it now")
+                sell_option(elt,pos[elt])
+                seen_opt.append(elt.symbol)
+
+            elif pnl < liquidation_treshhold_options and elt.symbol not in seen_opt and elt.symbol  in white_list:
+                print("      This stock is white listed")
+
+            elif pnl < liquidation_treshhold_options and elt.symbol in seen_opt  :
+                print("      This position is already closed")
 
 
         print("Checking again in 10 secs ")
